85b67d1e442b214e105ceb8af4cd3b347e068343
[Ankit Adhikari] <[ankitcsit25.oic.edu.np]>
2026-03-21 13:32:11 +0545

    added 3 C files

---
+++ add.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int a,b,sum;
+    printf("Enter a:");
+    scanf("%d", &a);
+    printf("Enter b:");
+    scanf("%d", &b);
+    sum = a + b;
+    printf("The sum of two numbers is %d", sum);
+    return 0;
+}
